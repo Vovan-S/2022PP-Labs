c7f5b845ee6fab5ecf343917d657dcd1513fef3e
Vladimir Sennov <thecaesarmagnus@gmail.com>
2022-10-08 23:17:01 +0300

    updated testing procedure

--- test/generate.py
+++ test/generate.py
@@ -1,26 +1,36 @@
 import numpy as np
 from visualize import plot_wave
+import utils
 
-def first_test(P, tau, M, steps):
-    h = 3 / M
-    T = P * tau 
-    res = np.zeros((P, M))
+
+DEFAULT_VALUES = [500, 500, 0.1, 0.1, 0.5]
+
+
+def generate_data(P, M, h, tau, a):
+    l = M * h
     t = np.linspace(
         np.zeros((M,)),
-        np.full((M,), T),
+        np.full((M,), P*tau),
         P,
         axis=0
     )
     x = np.linspace(
         np.zeros((P,)),
-        np.full((P,), 3),
+        np.full((P,), l),
         M,
         axis=1
     )
-    for n in range(1, steps + 1):
-        res += 18 / np.pi / n * np.cos(np.pi * n * t) * np.sin(np.pi * n * x / 3)
-    return res
+    return np.sin(np.pi * x / l)*np.sin(np.pi*a*t/l)
 
-x = first_test(300, 0.1, 100, 100)
 
-plot_wave(x, 3 / 100, 'test_wave.gif')
+def compare_data(filename):
+    actual = generate_data(*DEFAULT_VALUES)
+    got = utils.read_solution(filename)
+    diff = np.abs(actual - got)
+    print("File:", filename)
+    print("Average difference:", np.average(diff))
+    print("Max difference:", np.max(diff))
+
+
+if __name__ == "__main__":
+    compare_data("solution/numpy/res.txt")
